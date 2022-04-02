10659ed82ed05e801a2d1b75356551eccf06b868
Rodrigo Diniz da Silva <rodrigo_diniz_da_silva@yahoo.com>
2022-04-02 18:13:12 -0300

    Fiz mais um tema para o sistema Demonstração de Calendários, chamei de Tema Vermelho (tema_vermelho). Acrescentei a imagem Ícone de Calendário - Vermelho Sem Fundo 25x25.png, feita por mim, ao projeto.

--- PHP/template.php
+++ PHP/template.php
@@ -4,4 +4,4 @@ require_once '../Smarty/Smarty.class.php';
 
 $smarty = new Smarty;
 $smarty->assign('url_base_template', 'http://127.0.0.1/demonstracao_de_calendarios');
-$smarty->assign('tema_template', 'tema_azul');
+$smarty->assign('tema_template', 'tema_vermelho');
