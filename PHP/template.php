753a44da8a07e90097d6acfdebbdc5ce38ad9bff
Rodrigo Diniz da Silva <rodrigo_diniz_da_silva@yahoo.com>
2022-04-07 16:39:42 -0300

    Fiz mais um tema para o sistema Demonstração de Calendários, chamei de Tema Amarelo (tema_amarelo). Acrescentei a imagem Ícone de Calendário - Amarelo Sem Fundo 25x25.png, feita por mim, ao projeto.

--- PHP/template.php
+++ PHP/template.php
@@ -4,4 +4,4 @@ require_once '../Smarty/Smarty.class.php';
 
 $smarty = new Smarty;
 $smarty->assign('url_base_template', 'http://127.0.0.1/demonstracao_de_calendarios');
-$smarty->assign('tema_template', 'tema_verde');
+$smarty->assign('tema_template', 'tema_amarelo');
